7b145ca8a571f6bcfd3903d1c21de84c3007d7c8
Furqan Haroon <furqan@isoftstudios.com>
2023-02-09 18:51:00 +0500

    edit interfaces for up coming movies

--- src/app/interface/movie.interface.ts
+++ src/app/interface/movie.interface.ts
@@ -1,11 +1,11 @@
 export interface moviesObject {
-    poster_path: string;
-    img: string;
-    original_title: string;
-    _id:string;
-    overview:string;
-  vote_average:string;
-  title:string;
-  release_date:string;
-  vote_count:string;
-  }
+  poster_path: string;
+  img: string;
+  original_title: string;
+  _id: string;
+  overview: string;
+  vote_average: string;
+  title: string;
+  release_date: string;
+  vote_count: string;
+}
--- src/app/interface/user.interface.ts
+++ src/app/interface/user.interface.ts
@@ -1,28 +1,10 @@
+import { moviesObject } from "./movie.interface";
+
 export interface User {
 
         createdAt: string;
         email: string;
-        fav_movies: Array<{
-            adult: boolean;
-            backdrop_path: string;
-            createdAt: string;
-            genre_ids: number[];
-            id: number;
-            img: string;
-            original_language: string;
-            original_title: string;
-            overview: string;
-            popularity: number;
-            poster_path: string;
-            release_date: string;
-            title: string;
-            updatedAt: string;
-            video: boolean;
-            vote_average: number;
-            vote_count: number;
-            __v: number;
-            _id: string;
-        }>;
+        fav_movies: Array<moviesObject>;
         password: string;
         plan: string;
         plan_expire: string;
